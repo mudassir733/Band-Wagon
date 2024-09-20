93c725731eec41580b0e36424b0788db30e3d643
Mudassir Ali <deadlool45@gmail.com>
2024-09-20 16:19:28 +0500

    styles for onBoarding

---
+++ src/app/onboarding/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import OnboardingSlider from '../../components/onBoardingSlider/OnBoardingSlider'
+
+const OnBoardingPage = () => {
+  return (
+    <OnboardingSlider/>
+  )
+}
+
+export default OnBoardingPage
