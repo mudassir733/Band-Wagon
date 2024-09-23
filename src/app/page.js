94bdeca80bf0c43961677fd39f8297e7095b5704
Mudassir Ali <deadlool45@gmail.com>
2024-09-23 18:05:00 +0500

    Header Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
-import Button from "../components/buttons/Button";
-import FilledButton from "../components/buttons/FilledButton"
-import IconButton from "../components/buttons/IconButton"
-
+import Header from "../components/header/Header"
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
-      <h1>Welcome to Next.js!</h1>
-    </div>
+  <>
+      <Header/>
+  </>
+
+
   );
 }
