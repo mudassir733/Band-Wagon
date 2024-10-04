de1286d0bb19a9b4cd10ea95ce8f05f41c47be7b
mudassir733 <deadlool45@gmail.com>
2024-10-04 09:37:36 +0500

    artist page management ui

---
+++ src/app/artist-page-management/page.js
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import Header from "../../components/header/Header"
+import ArtistPageManagement from "../../components/artist-page-management/ArtistPageManagement"
+const page = () => {
+    return (
+        <div>
+            <Header />
+            <ArtistPageManagement />
+        </div>
+    )
+}
+
+export default page
