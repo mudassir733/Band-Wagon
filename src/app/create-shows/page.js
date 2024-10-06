87e786d483aff0cb0019de6b4a05aa40a33f746e
mudassir733 <deadlool45@gmail.com>
2024-10-06 11:45:11 +0500

    create show paga ui

---
+++ src/app/create-shows/page.js
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import Header from "../../components/header/Header"
+import CreateShows from "../../components/create-shows/CreateShows"
+
+const page = () => {
+    return (
+        <div>
+            <Header />
+            <CreateShows />
+        </div>
+    )
+}
+
+export default page
