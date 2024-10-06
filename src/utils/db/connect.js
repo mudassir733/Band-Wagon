a3403fb4204728063d6b926eb251ea0ed840ef63
mudassir733 <deadlool45@gmail.com>
2024-10-06 23:24:06 +0500

    connect to database

---
+++ src/utils/db/connect.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../../constants.js";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+        console.log(
+            `\n Connected to MongoDB !! DB HOST:`,
+            connectionInstance.connection.host
+        );
+    } catch (error) {
+        console.log("Error Connecting to MongoDB", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
