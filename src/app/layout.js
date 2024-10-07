46c3dda13c079dec0077d53d6398badd098bc9dc
mudassir733 <deadlool45@gmail.com>
2024-10-07 11:46:07 +0500

    updated layout for toast theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,7 @@
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export const metadata = {
   title: "Create Next App",
@@ -12,6 +15,7 @@ export default function RootLayout({ children }) {
         className={`antialiased`}
       >
         {children}
+        <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} closeOnClick pauseOnHover={false} draggable theme="colored" />
       </body>
     </html>
   );
