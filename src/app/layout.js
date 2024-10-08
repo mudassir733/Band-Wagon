21a4b7c36e1f424af1614fc46861f31023d9a61b
mudassir733 <deadlool45@gmail.com>
2024-10-08 11:39:17 +0500

    structure the root layout page for checking is user is already logged in

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,42 @@
-"use client"
+"use client";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
+const AuthChecker = ({ children }) => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
+  useEffect(() => {
+    if (status === "loading") return;
+    if (!session) {
+      router.push("/login");
+    }
+  }, [session, router, status]);
 
+  return <>{children}</>;
+};
 
 export default function RootLayout({ children }) {
   return (
     <SessionProvider>
       <html lang="en">
-        <body
-          className={`antialiased`}
-        >
-          {children}
-          <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} closeOnClick pauseOnHover={false} draggable theme="colored" />
+        <body className={`antialiased`}>
+          <AuthChecker>{children}</AuthChecker>
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            hideProgressBar={false}
+            closeOnClick
+            pauseOnHover={false}
+            draggable
+            theme="colored"
+          />
         </body>
       </html>
     </SessionProvider>
-
   );
 }
