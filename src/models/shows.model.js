664e99661be7825975c7e4fbfa77bb0d3b036d7f
mudassir733 <deadlool45@gmail.com>
2024-10-24 23:20:12 +0500

    creating the show model

---
+++ src/models/shows.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose"
+
+const ShowSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: false,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+    latitude: {
+        type: Number,
+        required: false,
+        default: null,
+    },
+    longitude: {
+        type: Number,
+        required: false,
+        default: null,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    bio: {
+        type: String,
+        required: true,
+    },
+    genres: {
+        type: [String],
+        enum: ['Blues', 'Classical', 'Country', 'EDM', 'Folk', 'Funk', 'Hip-Hop', 'Jazz', 'Latin', 'Metal', 'Pop', 'Punk', 'Reggae', 'R&B', 'Rock', 'Soul'],
+        required: false,
+    },
+    verified: {
+        type: Boolean,
+        default: false,
+        required: false,
+    }
+}, { timestamps: true });
+
+const Show = mongoose.models.Show || mongoose.model('Show', ShowSchema);
+
+export default Show;
